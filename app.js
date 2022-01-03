668a671d7040b52899ad63cc3a99e8cc1b8bc782
sunaycansev <fsunaycansev@gmail.com>
2022-01-04 00:26:15 +0300

    created workspace

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  const blog = {
+    id: 1,
+    title: "Blog title",
+    description: "Blog description",
+  };
+  res.send(blog);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
