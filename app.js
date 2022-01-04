5e6ffb17fcde9bd947f6e6730a0125caf4811a7f
sunaycansev <fsunaycansev@gmail.com>
2022-01-04 11:52:52 +0300

    add template engine | add views

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const ejs = require("ejs");
 
 const app = express();
-
+// Middlewares
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+// Routes
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add_post");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
 });
 
 const port = 3000;
